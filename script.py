f9e5b81200087e3f2c4e9f90caaa370f91a4d79e
ismaelnetodev <ismaelm.neto14@hotmail.com>
2023-05-26 11:20:10 -0300

    Interface básica criada
    Usando tkinter. Ainda há de melhorar

--- script.py
+++ script.py
@@ -3,18 +3,27 @@ import speech_recognition as sr  # pip install SpeechRecognition
 from key import IAKEY
 import pyttsx3  # pip install pyttsx3
 import os
+import tkinter
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
 
 # Initialize the API key
 openai.api_key = IAKEY
 
 # printa o total de tokens por interacao
-debug_custo = True
+debug_custo = False
 # print de algumas informacoes para debug
-debugar = True
+debugar = False
 escolher_stt = "google"
+
 # falar ou nao
 falar = True
 ativar_fala = False
+
+window = Tk()
+window.title("Solvix - chatbot inteligente")
+window.geometry("500x400")
 
 
 def generate_answer(messages):
@@ -34,18 +43,11 @@ def talk(texto):
     engine.runAndWait()
     engine.stop()
 
-
-def save_file(dados):
-    with open(path + filename, "wb") as f:
-        f.write(dados)
-        f.flush()
-
-
 # reconhecer
 r = sr.Recognizer()
 mic = sr.Microphone()
 
-# falar
+#falar
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('rate', 180)  # velocidade 120 = lento
@@ -54,45 +56,40 @@ for indice, vozes in enumerate(voices):  # listar vozes
 voz = 0  
 engine.setProperty('voice', voices[voz].id)
 
-mensagens = [{"role": "system", "content": "Você é um chat inteligente, seu nome é Solvix e quer ajudar resolver problemas. Caso pergutem quem são seu criadores: Seus criadores são: Ismael, Rosana e Átilla, Isadora e Aryelle. Caso pergutemo significado do seu nome: O nome 'Solvix'é uma combinação das palavras 'solve'(resolver, em inglês) e o sufixo '-i x'. O termo 'solve' é associado à ação de solucionar, encontrar respostas ou resolver problemas. Caso peça para se apresentar: Diga 'Olá turma', Diga seu nome, seus criadores e que foi projetado para um trabalho no IFMA sobre a tutela dos professores 'Akyra' e 'Franklin'"}]
-
-path = os.getcwd()
-filename = "audio.wav"
+mensagens = [{"role": "system", "content": "Você é um chat inteligente, seu nome é Solvix e quer ajudar resolver problemas. Caso pergutem quem são seu criadores: Seus criadores são: Ismael, Rosana e Átilla, Isadora e Aryelle. Caso pergutemo significado do seu nome: O nome 'Solvix'é uma combinação das palavras 'solve'(resolver, em inglês) e o sufixo '-ix'. O termo 'solve' é associado à ação de solucionar, encontrar respostas ou resolver problemas. Caso peça para se apresentar: Diga 'Olá turma', Diga seu nome, seus criadores e que foi projetado para um trabalho no IFMA sobre a tutela dos professores 'Akyra' e 'Franklin'"}]
 
 print("Speak to Text", escolher_stt)
+def reconhecerFala():
+    ajustar_ambiente_noise = True
 
-ajustar_ambiente_noise = True
+    while True:
+        text = ""
+        question = ""
+            # Ask a question
+        with mic as fonte:
+            if ajustar_ambiente_noise:
+                r.adjust_for_ambient_noise(fonte)
+                ajustar_ambiente_noise = False
+            print("Fale alguma coisa")
+            audio = r.listen(fonte)
+            print("Enviando para reconhecimento")
 
-while True:
-    text = ""
-    question = ""
+            if escolher_stt == "google":
+                try:
+                    question = r.recognize_google(audio, language="pt-BR")
+                except Exception as e:
+                    print("Erro no reconhecimento")
+                    continue
 
-        # Ask a question
-    with mic as fonte:
-        if ajustar_ambiente_noise:
-            r.adjust_for_ambient_noise(fonte)
-            ajustar_ambiente_noise = False
-        print("Fale alguma coisa")
-        audio = r.listen(fonte)
-        print("Enviando para reconhecimento")
-
-        if escolher_stt == "google":
-            try:
-                question = r.recognize_google(audio, language="pt-BR")
-            except Exception as e:
-                print("Erro no reconhecimento")
-                continue
-            
-    if ("desligar" in question and "assistente" in question) or question.startswith("sair"):
-        print(question, "Saindo.")
-        if falar:
-            talk("Desligando")
-        break
-    elif question == "":
-        print("No sound")
-        continue
-    elif question.startswith("Assistente") or question.startswith("assistente") or question.startswith(
-            "solvix"):
+        if ("desligar" in question and "assistente" in question) or question.startswith("sair"):
+            print(question, "Saindo.")
+            if falar:
+                talk("Desligando")
+            break
+        elif question == "":
+            print("No sound")
+            continue
+        
         print("Me: ", question)
         mensagens.append({"role": "user", "content": str(question)})
 
@@ -107,10 +104,15 @@ while True:
 
         if falar:
             talk(answer[0])
-    else:
-        print("No message")
-        continue
+        else:
+            print("No message")
+            continue
 
-    if debugar:
-        print("Mensages", mensagens, type(mensagens))
-print("See ya!")
+        if debugar:
+            print("Mensages", mensagens, type(mensagens))
+
+
+botao = Button(window, text="Falar", command=reconhecerFala)
+botao.pack()
+
+window.mainloop()
